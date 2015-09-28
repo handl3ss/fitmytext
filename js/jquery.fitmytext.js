2d8f6195f8d95addd33a6b7db6e1f5307c6812d7
Oleksandr Bezruchenko <bezruchenko.a@gmail.com>
2015-09-28 15:06:06 +0300

    last version

--- js/jquery.fitmytext.js
+++ js/jquery.fitmytext.js
@@ -6,26 +6,24 @@
  * @version   0.0.1
  * @copyright (c) 2015 Oleksandr Bezruchenko
  * @homepage  https://github.com/handl3ss/fitmytext
- * @example   https://fitmytext.github.com
+ * @example   https://handl3ss.github.io/fitmytext
  */
 (function($){
-  jQuery.fn.fittext = function(options){
-  jQuery.fn.fittext.options = settings;  
+  $.fn.fittext = function(options){
+  $.fn.fittext.options = settings;  
     var settings = $.extend({
      minFontSize: 5,
      maxFontSize: false //set 43 and its work correctly
     }, options);
 
-    this.each(function(){
-        
         var $div = $(this);
-        if(!jQuery.fn.fittext.standartDivFontSize){
+        if(!$.fn.fittext.standartDivFontSize){
                 var standartDivFontSize = parseFloat(settings.maxFontSize || $div.css('font-size'), 10);
-                jQuery.fn.fittext.standartDivFontSize = standartDivFontSize;  
+                $.fn.fittext.standartDivFontSize = standartDivFontSize;  
         } 
         var divFontSize = parseFloat(settings.maxFontSize || $div.css('font-size'), 10);
-        if(jQuery.fn.fittext.standartDivFontSize > divFontSize){
-            divFontSize =jQuery.fn.fittext.standartDivFontSize
+        if($.fn.fittext.standartDivFontSize > divFontSize){
+            divFontSize =$.fn.fittext.standartDivFontSize
         }
         var width   = $div.width();
         var clone   = $div.data('divFitTester-clone');
@@ -67,8 +65,7 @@
 
         }).triggerHandler('keyup.fittext');
   
-            
-    })
+
 
      return this;
 
